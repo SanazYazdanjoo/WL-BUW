2d3063cd293ad1f46f04151cc1d3dec5d5c5ab53
Sanaz Yazdanjoo <sanazyazdanju36@gmail.com>
2026-07-24 13:14:52 +0000

    Initial Setups

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-900 text-white">
+      <h1 className="text-3xl font-bold text-emerald-400">Tailwind is working!</h1>
+    </div>
+  )
+}
+
+export default App
